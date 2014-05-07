cf0285de491403879ef131a8fc203da9673d10eb
Lee-W <cl87654321@gmail.com>
2014-05-07 20:21:20 +0800

    add install-all.sh for quick install

---
+++ install-all.sh
@@ -0,0 +1,17 @@
+sh ppa-setting.sh
+
+sudo apt-get update 
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+
+sh basic_setup.sh
+sh oh-my-zsh-install.sh
+sh sdcv-install.sh
+sh application.sh    
+sh developer.sh
+sh macbuntu14-04.sh
+
+sudo apt-get -f install &&
+sudo apt-get autoremove &&
+sudo apt-get -y autoclean &&
+sudo apt-get -y clean
