ef7eacb15fa4768f9ceaad2eff7496714d910d30
Lee-W <cl87654321@gmail.com>
2014-05-07 20:21:00 +0800

    add developer for develop enviroment setting

---
+++ developer.sh
@@ -0,0 +1,9 @@
+#andorid develop enviroment
+sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0
+sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5
+
+#python pip
+##for python 2.7.x
+sudo apt-get install python-pip
+##for python 3.x
+sudo apt-get install python3-pip
