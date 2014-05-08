d10e54bf942f2eb1d814addbadba0aa70a5f5ccb
LeeW <cl87654321@gmail.com>
2014-05-09 01:43:22 +0800

    add auto confirm for basic-setup

--- basic-setup.sh
+++ basic-setup.sh
@@ -15,21 +15,21 @@ sudo apt-get install y-ppa-manager -y
 #advance power manangement
 sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update
-sudo apt-get install tlp tlp-rdw
+sudo apt-get install tlp tlp-rdw -y
 sudo tlp start
 
 #java
-sudo apt-get install icedtea-7-plugin openjdk-7-jre oracle-java8-installer
+sudo apt-get install icedtea-7-plugin openjdk-7-jre oracle-java8-installer -y
 
 #flash
-sudo apt-get install flashplugin-installer
+sudo apt-get install flashplugin-installer -y
 
 #media restriction
 sudo apt-get install libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras -y
 
 #tweak
-sudo apt-get install unity-tweak-tool
-sudo apt-get install gnome-tweak-tool
+sudo apt-get install unity-tweak-tool -y
+sudo apt-get install gnome-tweak-tool -y
 
 
 #other
