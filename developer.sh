350ec5dc1f6dd3f9be156108c78e4e78cf3dc692
LeeW <cl87654321@gmail.com>
2014-05-09 07:42:24 +0800

    add sublime-text

--- developer.sh
+++ developer.sh
@@ -7,3 +7,6 @@ sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 -y
 sudo apt-get install python-pip -y
 ##for python 3.x
 sudo apt-get install python3-pip -y
+
+#sublime 3
+sudo apt-get install sublime-text -y
