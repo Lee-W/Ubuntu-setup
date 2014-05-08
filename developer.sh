f0421bcea40858a6544293d7caef89001dddceb7
LeeW <cl87654321@gmail.com>
2014-05-08 08:27:29 +0800

    add auto confirm for developer.sh

--- developer.sh
+++ developer.sh
@@ -1,9 +1,9 @@
 #andorid develop enviroment
-sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0
-sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5
+sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 -y
+sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 -y
 
 #python pip
 ##for python 2.7.x
-sudo apt-get install python-pip
+sudo apt-get install python-pip -y
 ##for python 3.x
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
