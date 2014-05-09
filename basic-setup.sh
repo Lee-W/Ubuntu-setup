627bef2407ee515e8b7205add3d76d543fcac7f6
Lee-W <cl87654321@gmail.com>
2014-05-09 18:54:08 +0800

    refien basic-setup

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,10 +1,17 @@
+#basic develop tool
+sudo apt-get install vim -y
+sudo apt-get install git -y
+sudo apt-get install g++ -y
+#java
+sudo apt-get install icedtea-7-plugin openjdk-7-jre oracle-java8-installer -y
+
 #terminal related
+sudo apt-get install terminator -y
+sudo apt-get install guake -y
 sudo apt-get install zsh -y
 sudo apt-get install autojump -y
 sudo apt-get install tmux -y
 sudo apt-get install nautilus-open-terminal  -y
-sudo apt-get install terminator -y
-sudo apt-get install guake -y
 
 #package management
 sudo apt-get install synaptic -y
@@ -18,23 +25,16 @@ sudo apt-get update
 sudo apt-get install tlp tlp-rdw -y
 sudo tlp start
 
-#java
-sudo apt-get install icedtea-7-plugin openjdk-7-jre oracle-java8-installer -y
+#media restriction
+sudo apt-get install libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras -y
+
+#other
+sudo apt-get install unrar -y
+sudo apt-get install gcin -y
 
 #flash
 sudo apt-get install flashplugin-installer -y
 
-#media restriction
-sudo apt-get install libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras -y
-
 #tweak
 sudo apt-get install unity-tweak-tool -y
 sudo apt-get install gnome-tweak-tool -y
-
-
-#other
-sudo apt-get install vim -y
-sudo apt-get install git -y
-sudo apt-get install g++ -y
-sudo apt-get install unrar -y
-sudo apt-get install gcin -y
