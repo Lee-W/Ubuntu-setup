df67b0ed37c83425f283b8ce7706087504ecd667
LeeW <cl87654321@gmail.com>
2014-06-10 00:42:33 +0800

    fix auto confirm

--- sh/developer.sh
+++ sh/developer.sh
@@ -30,4 +30,4 @@ sudo apt-get install atom -y
 
 #----------lib-----------
 ##C++ qt4
-sudo apt-get install qt4-dev-tools qt4-doc qt4-qtconfig qt4-demos qt4-designer
+sudo apt-get install qt4-dev-tools qt4-doc qt4-qtconfig qt4-demos qt4-designer -y
