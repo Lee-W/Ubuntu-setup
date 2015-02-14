c8c8836fc20b3e30594d426bb90c8172a67287a4
LeeW <cl87654321@gmail.com>
2015-02-14 17:27:52 +0800

    add sdcv dic to repo

--- sh/sdcv-install.sh
+++ sh/sdcv-install.sh
@@ -1,12 +1,5 @@
 #sdcv
 sudo apt-get install sdcv -y
-# 朗道英漢字典
-wget -P /tmp http://abloz.com/huzheng/stardict-dic/zh_TW/stardict-langdao-ec-big5-2.4.2.tar.bz2
-# 朗道漢英字典
-wget -P /tmp http://abloz.com/huzheng/stardict-dic/zh_TW/stardict-langdao-ce-big5-2.4.2.tar.bz2
 
-mkdir -p $HOME/.stardict/dic
-cd /tmp
-# 將字典檔解壓縮至 $HOME/.stardict/dic
-tar jxvf stardict-langdao-ce-big5-2.4.2.tar.bz2 -C $HOME/.stardict/dic
-tar jxvf stardict-langdao-ec-big5-2.4.2.tar.bz2 -C $HOME/.stardict/dic
+mkdir -p $HOME/.stardict
+tar -C ~/.stardict/ -zxvf ../res/dic.tgz
